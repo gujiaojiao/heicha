620fe2327fea63927b90af276b0630a004dcef9b
Gujiaojiao <gjj19992021@163.com>
2025-08-05 16:59:39 +0800

    新增取餐页

--- pages/order/index.vue
+++ pages/order/index.vue
@@ -1,23 +1,71 @@
 <template>
-    <view class="order">
-		<!-- 顶部标签 -->
-		<view class="selectStore">
-			<text>选择门店</text>
+    <view class="order" :style="{ paddingTop: safeAreaInsets.top + 'px' }">
+		<!-- 顶部名称 -->
+		<view class="orderTitle">1点点</view>
+		<!-- tab切换 -->
+		<view class="menu-tabs">
+			<view :class="['tab', activeTab === 0 ? 'active' : '']" @click="activeTab = 0">当前订单</view>
+			<view :class="['tab', activeTab === 1 ? 'active' : '']" @click="activeTab = 1">历史订单</view>
 		</view>
-        <text>订单页面</text>
+		<!-- 当前订单 -->
+		<view class="currentOrder" v-if="activeTab===0">
+			lalalal
+		</view>
+		<!-- 历史订单 -->
+		<view class="historyOrder" v-if="activeTab===1">
+			bububub
+		</view>
     </view>
 </template>
 
 <script setup lang="ts">
+import {ref,computed,onMounted} from 'vue'
+
+const safeAreaInsets = (uni.getSystemInfoSync && uni.getSystemInfoSync().safeAreaInsets||{top:0})
+const activeTab=ref(0)
+
 </script>
 
 <style lang="scss" scoped>
 .order {
-    padding: 20rpx;
+    background: #f7f7f7;
+    min-height: 100vh;
 	
-	.selectStore{
+	.orderTitle{
 		display: flex;
-		text-align: center;
+		justify-content: center;
+		font-size: 16px;
+	}
+	.menu-tabs {
+		width: 100%;
+		display: flex;
+		justify-content: space-around;
+		border-bottom: 1px solid #eee;
+		margin: 10px 0;
+		.tab {
+			flex: 1;
+			text-align: center;
+			padding: 12px 0 8px 0;
+			font-size: 16px;
+			color: #888;
+			position: relative;
+			
+			&.active{
+				color: #1aad19;
+				font-weight: bold;
+				&::after {
+					content: '';
+					display: block;
+					width: 60%;
+					height: 3px;
+					background: #1aad19;
+					border-radius: 2px;
+					position: absolute;
+					left: 20%;
+					bottom: -1px;
+				}
+			}
+		}
 	}
 }
 </style>
