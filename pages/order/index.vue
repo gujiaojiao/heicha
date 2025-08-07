e29f9f69d2dcf26803613f5f1146d8b209ad6415
Gujiaojiao <gjj19992021@163.com>
2025-08-07 17:01:01 +0800

    历史订单页面更新

--- pages/order/index.vue
+++ pages/order/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <view class="order" :style="{ paddingTop: safeAreaInsets.top + 'px' }">
+	<view class="order" :style="{ paddingTop: safeAreaInsets.top + 'px' }">
 		<!-- 顶部名称 -->
 		<view class="orderTitle">1点点</view>
 		<!-- tab切换 -->
@@ -8,40 +8,70 @@
 			<view :class="['tab', activeTab === 1 ? 'active' : '']" @click="activeTab = 1">历史订单</view>
 		</view>
 		<!-- 当前订单 -->
-		<view class="currentOrder" v-if="activeTab===0">
+		<view class="currentOrder" v-if="activeTab === 0">
 			lalalal
 		</view>
 		<!-- 历史订单 -->
-		<view class="historyOrder" v-if="activeTab===1">
-			bububub
+		<view class="historyOrder" v-if="activeTab === 1"
+			:style="{ height: `calc(100vh - ${safeAreaInsets.top + 30 + 40}px)` }">
+			<view v-for="item in historyOrder" :key="item.id">
+				<view class="historyOrder__item">
+					<view class="historyOrder__item__left">
+						<view class="historyOrder__item__left__title">{{ item.storeAddress }}</view>
+					</view>
+				</view>
+			</view>
 		</view>
-    </view>
+	</view>
 </template>
 
 <script setup lang="ts">
-import {ref,computed,onMounted} from 'vue'
+import { ref, computed, onMounted } from 'vue'
+import { getCurrentOrder, getOrderHistory } from '@/utils/api/order-mock'
 
-const safeAreaInsets = (uni.getSystemInfoSync && uni.getSystemInfoSync().safeAreaInsets||{top:0})
-const activeTab=ref(0)
+const safeAreaInsets = (uni.getSystemInfoSync && uni.getSystemInfoSync().safeAreaInsets || { top: 0 })
+const activeTab = ref(0)
+
+onMounted(() => {
+	if (activeTab.value === 0) {
+		getCurrentOrder().then(res => {
+			console.log(1111111, res)
+		})
+	} else {
+		getOrderHistory().then(res => {
+			console.log(222222, res)
+		})
+	}
+})
+
 
 </script>
 
 <style lang="scss" scoped>
 .order {
-    background: #f7f7f7;
-    min-height: 100vh;
-	
-	.orderTitle{
+	background: #f7f7f7;
+	min-height: 100vh;
+
+	.orderTitle {
 		display: flex;
 		justify-content: center;
 		font-size: 16px;
+		width: 100%;
+		height: 30px;
+		line-height: 30px;
+		flex-shrink: 0;
 	}
+
 	.menu-tabs {
+
 		width: 100%;
 		display: flex;
 		justify-content: space-around;
 		border-bottom: 1px solid #eee;
 		margin: 10px 0;
+		height: 40px;
+		flex-shrink: 0;
+
 		.tab {
 			flex: 1;
 			text-align: center;
@@ -49,10 +79,11 @@ const activeTab=ref(0)
 			font-size: 16px;
 			color: #888;
 			position: relative;
-			
-			&.active{
+
+			&.active {
 				color: #1aad19;
 				font-weight: bold;
+
 				&::after {
 					content: '';
 					display: block;
@@ -65,6 +96,22 @@ const activeTab=ref(0)
 					bottom: -1px;
 				}
 			}
+		}
+	}
+
+	.historyOrder {
+		margin: 15px 10px;
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		overflow-y: auto;
+		background-color: #888;
+		// height: calc(100vh - 100px);
+
+		.historyOrder__item {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
 		}
 	}
 }
---
+++ utils/api/order-mock.ts
@@ -0,0 +1,120 @@
+// 订单
+export interface OrderProduct {
+    name: string;
+    imageUrl: string;
+    desc: string;
+    count: number;
+    price: number;
+  }
+  
+  // 当前订单类型
+export interface CurrentOrder {
+    id: number;
+    storeAddress: string;    // 店名
+    takeNumber: string;      // 取餐号
+    totalCups: number;       // 本单总杯数
+    products: OrderProduct[];// 订单详情
+    storeTotalCups: number;  // 店家当前总共要完成的杯数
+    waitCups: number;        // 还剩余几杯到你
+    status: string;          // 完成状态（如“制作中”、“待取餐”、“已完成”）
+  }
+//   历史订单类型
+  export interface OrderHistory {
+    id: number;
+    storeAddress: string;
+    orderTime: string;
+    status: string;
+    type: string;
+    products: OrderProduct[];
+    totalPrice: number;
+    buttonText: string;
+  }
+
+  // 获取历史订单列表
+export const getOrderHistory = (): Promise<OrderHistory[]> => {
+  return Promise.resolve([
+    {
+      id: 1,
+      storeAddress: "苏州创业园店",
+      orderTime: "2025-07-23 12:18:12",
+      status: "已完成",
+      type: "自取",
+      products: [
+        {
+          name: "四季奶青",
+          imageUrl: "/static/images/index/drink.png",
+          desc: "中杯/去冰/不另外加糖/波霸/椰果",
+          count: 1,
+          price: 0.00
+        }
+      ],
+      totalPrice: 0.00,
+      buttonText: "再来一单"
+    },
+    {
+      id: 2,
+      storeAddress: "苏州创业园店",
+      orderTime: "2025-07-16 13:34:00",
+      status: "已完成",
+      type: "自取",
+      products: [
+        {
+          name: "四季奶青",
+          imageUrl: "/static/images/index/drink.png",
+          desc: "中杯/常温/不另外加糖/波霸",
+          count: 1,
+          price: 0.00
+        }
+      ],
+      totalPrice: 0.00,
+      buttonText: "再来一单"
+    },
+    {
+      id: 3,
+      storeAddress: "苏州创业园店",
+      orderTime: "2025-06-20 13:35:26",
+      status: "已完成",
+      type: "自取",
+      products: [
+        {
+          name: "四季奶青",
+          imageUrl: "/static/images/index/drink.png",
+          desc: "中杯/常温/不另外加糖/波霸",
+          count: 1,
+          price: 0.00
+        }
+      ],
+      totalPrice: 0.00,
+      buttonText: "再来一单"
+    }
+  ])
+}
+
+// 获取当前订单
+export const getCurrentOrder = (): Promise<CurrentOrder> => {
+  return Promise.resolve({
+    id: 101,
+    storeAddress: "苏州创业园店",
+    takeNumber: "A023",
+    totalCups: 2,
+    products: [
+      {
+        name: "四季奶青",
+        imageUrl: "/static/images/index/drink.png",
+        desc: "大杯/去冰/波霸",
+        count: 1,
+        price: 12.00
+      },
+      {
+        name: "金凤茶王",
+        imageUrl: "/static/images/index/drink.png",
+        desc: "中杯/常温/椰果",
+        count: 1,
+        price: 13.00
+      }
+    ],
+    storeTotalCups: 8,   // 店家当前总共要做8杯
+    waitCups: 3,         // 还剩3杯到你
+    status: "制作中"      // 也可以是“待取餐”、“已完成”
+  })
+}
